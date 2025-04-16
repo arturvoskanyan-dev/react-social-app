31469c9ddc1689891b0b31a3dbfcdea16c5d5334
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-04-16 19:26:17 +0400

    feat : Add changePhoto functionality

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0",
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        "API-KEY": "e5d304c3-76cc-49a8-a021-9a1e7009afe8",
+    }
 })
 
 export const SocialAPI = {
@@ -16,5 +19,11 @@ export const SocialAPI = {
 
     setLogin(email, password) {
         return instance.post(`/auth/login`, {email, password})
+    },
+
+    changePhoto(file) {
+        let formData = new FormData();
+        formData.append("file", file)
+        return instance.put("profile/photo", formData)
     }
 }
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -3,11 +3,17 @@ import { useParams } from 'react-router-dom'
 import { userThunk } from '../../store/action/userAction';
 import { useDispatch, useSelector } from 'react-redux';
 import userImage from "../../assets/user.png"
+import { SocialAPI } from '../../api/api';
 
 export default function UserProfile() {
     let { id } = useParams();
     const dispatch = useDispatch();
-    const {user} = useSelector((state) => state.userPage);
+    const { user } = useSelector((state) => state.userPage);
+
+    const changeProfile = (e) => {
+        const files = e.target.files[0];
+        SocialAPI.changePhoto(files)
+    }
 
     useEffect(() => {
         dispatch(userThunk(id))
@@ -17,13 +23,19 @@ export default function UserProfile() {
         <section className='p-30 flex items-center justify-between'>
             <div className='flex items-center gap-4'>
                 <img src={user?.photos?.large || userImage} className="rounded-full" />
-                <h2 className='text-xl font-bold'>{user?.fullName}</h2>
+                <div className='flex flex-col gap-4'>
+                    <h2 className='text-xl font-bold'>{user?.fullName}</h2>
+                    {
+                        localStorage.getItem("userId") === id
+                        && <input
+                            type="file"
+                            onChange={changeProfile}
+                            placeholder=''
+                            className='p-2 font-bold bg-gray-300'
+                        />
+                    }
+                </div>
             </div>
-            {/* <div>
-                <button className='bg-primary-bg w-[200px] p-2 text-white font-medium rounded-xl cursor-pointer hover:bg-blue-700'>
-                    Follow
-                </button>
-            </div> */}
         </section>
     )
 }
